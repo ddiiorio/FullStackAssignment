b9f6c48f5755f706c8679f8d643d7a33c597acd7
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-02-09 15:06:13 -0800

    Setup nav bar and bootstrap

--- comp4870-a1-frontend/src/app/app.component.ts
+++ comp4870-a1-frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'comp4870-a1-frontend';
+  title = 'Comp4870 Assignment 1';
 }
---
+++ comp4870-a1-frontend/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    Username: string
+    Email: string
+    Password: string
+    FirstName: string
+    LastName: string
+    Country: string
+    MobileNumber: string
+}
+
